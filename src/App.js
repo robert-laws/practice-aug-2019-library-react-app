8f949a091136dba2e85ee1732b1d6ae6ed774fb6
Robert Laws <roblaws@me.com>
2019-09-01 12:30:13 +0300

    testing - material-ui components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 import React from 'react';
-import { Container, Button, Typography } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { Box, TextField } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
 import './App.scss';
 
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
+
 function App() {
+  const classes = useStyles();
+  
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Library App
-      </Typography>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <InfoOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Lesson Plan
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField name="courseNumber" variant="outlined" required fullWidth id="courseNumber" label="Course Number" autoFocus />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField name="courseTitle" variant="outlined" required fullWidth id="courseTitle" label="Course Title" />
+            </Grid>
+          </Grid>
+        </form>
+      </div>
     </Container>
   );
 }
